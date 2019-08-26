ed766fac160249d92e78635b3b322f3343701213
Matthew X. Soyka <matthew.x.soyka@supervalu.com>
2019-08-26 13:47:32 -0500

    Updated package.json

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.tsx</code> and save to reloads. This is a new line
+          Edit <code>src/App.tsx</code> and save to reloads.
         </p>
         <a
           className='App-link'
