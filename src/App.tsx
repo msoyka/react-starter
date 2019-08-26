b887c5e47b9f212b6db8c107279d145d5fe175aa
Matthew X. Soyka <matthew.x.soyka@supervalu.com>
2019-08-26 13:44:42 -0500

    Added first files

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
         <p>
-          Edit <code>src/App.tsx</code> and save to reloads.
+          Edit <code>src/App.tsx</code> and save to reloads. This is a new line
         </p>
         <a
           className='App-link'
